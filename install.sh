216ba55a691fe59541d3ed2fc8f29b0675aebd80
jenkins_ci <jenkins_ci@limxdynamics.com>
2024-08-21 09:45:36 +0800

    1.1.5.20240821092121

--- install.sh
+++ install.sh
@@ -69,9 +69,9 @@ install_basic_tools() {
     sudo apt update
     check_err "${yellow}更新软件包列表失败"
 
-    # Install Python, pip, wget, git
-    sudo apt install -y python3 python3-pip wget git
-    check_err "${yellow}安装Python, pip, wget, git失败"
+    # Install Python, pip, wget
+    sudo apt install -y python3 python3-pip wget
+    check_err "${yellow}安装Python, pip, wget失败"
 
     # Call the function
     configure_pip_source
